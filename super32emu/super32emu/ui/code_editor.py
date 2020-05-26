7560fd9e416e1c81e1c49b5c948efc7c4ff0907d
Thomas M. Schöller <7847075+MaxAtoms@users.noreply.github.com>
2020-05-26 15:04:55 +0200

    Added line numbers to code editor (#12)

---
+++ super32emu/super32emu/ui/code_editor.py
@@ -0,0 +1,97 @@
+from PySide2.QtCore import SIGNAL, QRect
+from PySide2.QtGui import QColor, Qt, QTextFormat, QPainter
+from PySide2.QtWidgets import QPlainTextEdit, QTextEdit
+from .line_number_area import *
+
+
+class CodeEditor(QPlainTextEdit):
+    """
+    CodeEditor with line numbers
+
+    https://doc.qt.io/qt-5/qtwidgets-widgets-codeeditor-example.html
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.lineNumberArea = LineNumberArea(self)
+
+        self.connect(self, SIGNAL('blockCountChanged(int)'), self.updateLineNumberAreaWidth)
+        self.connect(self, SIGNAL('updateRequest(QRect,int)'), self.updateLineNumberArea)
+        self.connect(self, SIGNAL('cursorPositionChanged()'), self.highlightCurrentLine)
+
+        # It is necessary to calculate the line number area width when the editor is created
+        self.updateLineNumberAreaWidth(0)
+
+    def lineNumberAreaWidth(self):
+        """
+        The lineNumberAreaWidth() function calculates the width of the LineNumberArea widget.
+        We take the number of digits in the last line of the editor and multiply that with the maximum width of a digit.
+        """
+        digits = 1
+        count = max(1, self.blockCount())
+        while count >= 10:
+            count /= 10
+            digits += 1
+        space = 3 + self.fontMetrics().width('9') * digits
+        return space
+
+    def updateLineNumberAreaWidth(self, _):
+        self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0)
+
+    def updateLineNumberArea(self, rect, dy):
+        if dy:
+            self.lineNumberArea.scroll(0, dy)
+        else:
+            self.lineNumberArea.update(0, rect.y(), self.lineNumberArea.width(),
+                                       rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.updateLineNumberAreaWidth(0)
+
+    def resizeEvent(self, event):
+        """
+        When the size of the editor changes, we also need to resize the line number area
+        """
+        super().resizeEvent(event)
+
+        cr = self.contentsRect()
+        self.lineNumberArea.setGeometry(QRect(cr.left(), cr.top(),
+                                              self.lineNumberAreaWidth(), cr.height()))
+
+    def lineNumberAreaPaintEvent(self, event):
+        painter = QPainter(self.lineNumberArea)
+
+        painter.fillRect(event.rect(), Qt.lightGray)
+
+        block = self.firstVisibleBlock()
+        blockNumber = block.blockNumber()
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        bottom = top + self.blockBoundingRect(block).height()
+
+        height = self.fontMetrics().height()
+        while block.isValid() and (top <= event.rect().bottom()):
+            if block.isVisible() and (bottom >= event.rect().top()):
+                number = str(blockNumber + 1)
+                painter.setPen(Qt.black)
+                painter.drawText(0, top, self.lineNumberArea.width(), height,
+                                   Qt.AlignRight, number)
+
+            block = block.next()
+            top = bottom
+            bottom = top + self.blockBoundingRect(block).height()
+            blockNumber += 1
+
+    def highlightCurrentLine(self):
+        extraSelections = []
+
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+
+            lineColor = QColor(Qt.yellow).lighter(160)
+
+            selection.format.setBackground(lineColor)
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extraSelections.append(selection)
+        self.setExtraSelections(extraSelections)
